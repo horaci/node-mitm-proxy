fa74969c98b0feec555377f40debfc5b7b9ebe2b
Horaci Cuevas <horaci@gmail.com>
2011-11-24 12:02:43 +0000

    added readme.md and package.json

--- examples/simple_processor.js
+++ examples/simple_processor.js
@@ -8,20 +8,19 @@ simpleProcessor = function(proxy) {
   proxy.on('request', function(request, req_url) {
     url = req_url;
     console.log("[" + url.hostname + url.pathname + "] - Processor request event, url: " + URL.format(req_url));
-    
   })
 
-	proxy.on('response', function(response) {
+  proxy.on('response', function(response) {
     console.log("[" + url.hostname + url.pathname + "] - Processor response event, response server header: " + response.headers.server);
-	});
+  });
 
-	proxy.on('response_data', function(data) {
+  proxy.on('response_data', function(data) {
    console.log("[" + url.hostname + url.pathname + "] - Processor response data event, length: " + data.length);
-	});
+  });
 
-	proxy.on('response_end', function() {
+  proxy.on('response_end', function() {
     console.log("[" + url.hostname + url.pathname + "] - Processor response end event");
-	});
+  });
 };
 
 // Proxy
